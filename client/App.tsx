abc362f1e07d8a8b04bfc6de8bdaa1443512d021
Jheavi <javierlaso213@hotmail.com>
2020-11-29 17:11:22 +0100

    rendered header

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import Workout from './src/components/Workout/Workout'
 import configureStore from './src/redux/configureStore'
+import Workout from './src/components/Workout/Workout'
+import Header from './src/components/Header/Header'
 
 const store = configureStore({})
 
 export default function App () {
   return (
     <Provider store={store}>
+      <Header />
       <Workout />
     </Provider>
   )
