61d5ea7be52d7bd969017134fac485a1516a3601
Jheavi <javierlaso213@hotmail.com>
2020-11-29 02:39:32 +0100

    created first action and action interface

---
+++ client/src/redux/actions/action-types.ts
@@ -0,0 +1,6 @@
+const actionTypes = {
+  LOAD_WORKOUT: 'LOAD_WORKOUT',
+  LOAD_WORKOUT_ERROR: 'LOAD_WORKOUT_ERROR'
+}
+
+export default actionTypes
---
+++ client/src/redux/actions/workoutActions.ts
@@ -0,0 +1,29 @@
+import actionTypes from './action-types'
+import axios from 'axios'
+import serverUrls from '../constants/serverUrls'
+
+export function loadWorkoutSuccess (workout: object) {
+  return {
+    type: actionTypes.LOAD_WORKOUT,
+    workout
+  }
+}
+
+export function loadWorkoutError (error: any) {
+  return {
+    type: actionTypes.LOAD_WORKOUT_ERROR,
+    error
+  }
+}
+
+export function loadWorkout (day: string) {
+  return async (dispatch: Function) => {
+    try {
+      const { data } = await axios.get(serverUrls.workoutUrl, { params: day })
+
+      dispatch(loadWorkoutSuccess(data))
+    } catch (error) {
+      dispatch(loadWorkoutError(error))
+    }
+  }
+}
