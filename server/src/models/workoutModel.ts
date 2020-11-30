3361457205782155d21ba7deb4b41a5e21771313
Jheavi <javierlaso213@hotmail.com>
2020-11-30 18:23:39 +0100

    created schema interface

--- server/src/models/workoutModel.ts
+++ server/src/models/workoutModel.ts
@@ -2,8 +2,16 @@ const mongoose = require('mongoose')
 
 const { Schema, model } = mongoose
 
-const workoutSchema = new Schema({
+export interface workout {
+  description: string,
+  gym: string,
+  date: string,
+  type: string
+}
+
+const workoutSchema: workout = new Schema({
   description: String,
+  title: String,
   gym: { type: Schema.Types.ObjectId, ref: 'gym' },
   date: String,
   type: String
