c6216bfdcc83ac135ebfe7e19fc3412591ca2fe6
Jheavi <javierlaso213@hotmail.com>
2020-11-30 18:25:17 +0100

    created extractDataFromDate function

--- client/src/utils/dateFunctions.ts
+++ client/src/utils/dateFunctions.ts
@@ -1,10 +1,16 @@
-import dateObject from './dateInterface'
+import { dateObject } from '../interfaces/interfaces'
 
-export function todayData (): dateObject {
+export function extractDataFromTodayDate (): dateObject {
   const today: Date = new Date()
   const day: number = today.getDate()
   const month: number = today.getMonth() + 1
   const year: number = today.getFullYear()
 
   return { day, month, year, todayString: `${year}-${month}-${day}` }
+}
+
+export function extractDataFromDate (date: string) {
+  const [year, month, day] = date.split('-')
+
+  return { year: +year, month: +month, day: +day }
 }
--- client/src/utils/dateInterface.ts
+++
@@ -1,6 +0,0 @@
-export default interface dateObject {
-  day: number,
-  month: number,
-  year: number,
-  todayString: string,
-}
