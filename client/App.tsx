465fd50cd2b8b95d6501e522340476c88a80c96d
Jheavi <javierlaso213@hotmail.com>
2020-12-01 17:34:18 +0100

    created initial navigation and homeScreen

--- client/App.tsx
+++ client/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import configureStore from './src/redux/configureStore'
 import Workout from './src/components/Workout/Workout'
 import Header from './src/components/Header/Header'
 
 const store = configureStore({})
+const { Navigator, Screen } = createStackNavigator()
 
 export default function App () {
   return (
     <Provider store={store}>
       <Header />
-      <Workout />
+      <NavigationContainer>
+        <Navigator initialRouteName="Home">
+          <Screen name="Home" component={Workout} />
+        </Navigator>
+      </NavigationContainer>
     </Provider>
   )
 }
