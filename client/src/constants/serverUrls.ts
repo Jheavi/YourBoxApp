6f761b5b8ebf3458c8fdd0f759d6a72c8f1a7178
Jheavi <javierlaso213@hotmail.com>
2020-12-02 00:14:00 +0100

    created url for schedules requests

--- client/src/constants/serverUrls.ts
+++ client/src/constants/serverUrls.ts
@@ -1,5 +1,6 @@
 const serverUrls = {
-  workoutUrl: 'http://192.168.43.206:2130/workouts'
+  workoutUrl: 'http://192.168.43.206:2130/workouts',
+  scheduleUrl: 'http://192.168.43.206:2130/schedules'
 }
 
 export default serverUrls
