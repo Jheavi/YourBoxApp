10ff6daa56bcc77cc7f171d482a3505ee33fb68a
Jheavi <javierlaso213@hotmail.com>
2020-12-01 16:45:52 +0100

    changes to make correct typescript

--- client/src/redux/actions/action-types.ts
+++ client/src/redux/actions/action-types.ts
@@ -1,4 +1,11 @@
-const actionTypes = {
+interface actionTypesInterface {
+  LOAD_WORKOUT: string
+  LOAD_WORKOUT_ERROR: string
+  UPDATE_WORKOUT: string
+  UPDATE_WORKOUT_ERROR: string
+}
+
+const actionTypes: actionTypesInterface = {
   LOAD_WORKOUT: 'LOAD_WORKOUT',
   LOAD_WORKOUT_ERROR: 'LOAD_WORKOUT_ERROR',
   UPDATE_WORKOUT: 'UPDATE_WORKOUT',
