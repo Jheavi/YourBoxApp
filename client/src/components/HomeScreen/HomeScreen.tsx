fb8d68b5aa3d26c45e471a840d6561eed46ac846
Jheavi <javierlaso213@hotmail.com>
2020-12-01 19:43:29 +0100

    created basic navigation with header

--- client/src/components/HomeScreen/HomeScreen.tsx
+++ client/src/components/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
+import PropTypes from 'prop-types'
 
-function HomeScreen () {
+function HomeScreen ({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('AdminWorkout')}
+      />
     </View>
   )
+}
+
+HomeScreen.propTypes = {
+  navigation: PropTypes.object.isRequired
 }
 
 export default HomeScreen
