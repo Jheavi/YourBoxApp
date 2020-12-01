7747a64b605a7a950930c99f1f7561001cfa8c82
Jheavi <javierlaso213@hotmail.com>
2020-12-02 00:54:53 +0100

    reducer order schedules start hours

--- client/src/redux/reducers/schedulesReducer.ts
+++ client/src/redux/reducers/schedulesReducer.ts
@@ -9,9 +9,13 @@ interface schedulesReducerInterface {
 const initialState = {}
 
 export default function schedulesReducer (state: schedulesReducerInterface = initialState, action: Action) {
+  let schedulesWithOrderedHours
   switch (action.type) {
     case actionTypes.LOAD_SCHEDULES:
-      return { ...state, schedules: action.schedules }
+      schedulesWithOrderedHours = action.schedules!.map((schedule) => {
+        return { ...schedule, hours: schedule.hours.sort((hour1, hour2) => hour1.startHour >= hour2.startHour ? 1 : -1) }
+      })
+      return { ...state, schedules: schedulesWithOrderedHours }
     default:
       return state
   }
