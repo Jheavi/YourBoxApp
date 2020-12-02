521b3d67d388b695086bcea47b6fceb2e989236d
Jheavi <javierlaso213@hotmail.com>
2020-12-02 20:57:34 +0100

    added create session button

--- client/src/components/Schedules/DaySchedule/DaySchedule.tsx
+++ client/src/components/Schedules/DaySchedule/DaySchedule.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { dayScheduleProps, sessionInterface } from '../../../interfaces/interfaces'
 import HourItem from '../HourItem/HourItem'
 
 const { width } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
+  dayView: {
+    flex: 1,
+    width: width - 10,
+    marginHorizontal: 5,
+    position: 'relative'
+  },
   dayText: {
     fontSize: 24,
     color: 'white',
@@ -23,10 +29,25 @@ const styles = StyleSheet.create({
     paddingHorizontal: 70,
     lineHeight: 40
   },
-  dayView: {
-    flex: 1,
-    width: width - 10,
-    marginHorizontal: 5
+  createButton: {
+    position: 'absolute',
+    right: 10,
+    top: 10,
+    backgroundColor: '#cb1313',
+    minWidth: 40,
+    minHeight: 40,
+    borderRadius: 4,
+    elevation: 5,
+    zIndex: 100
+  },
+  createButtonText: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: 'white'
+  },
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
 
@@ -34,6 +55,9 @@ function DaySchedule ({ weekDay }: dayScheduleProps) {
   return (
     <View style={styles.dayView}>
       <Text style={styles.dayText}>{weekDay.day}</Text>
+      <TouchableOpacity style={styles.createButton}>
+        <Text style={styles.createButtonText}>+</Text>
+      </TouchableOpacity>
       <View>
         {weekDay && (!weekDay.hours.length
           ? <Text style={styles.noScheduleText}>There is no schedule for this day</Text>
