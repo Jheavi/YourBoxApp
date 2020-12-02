54f6dd680879e7ffb9ae1074fff36f0bf7edbbb0
Jheavi <javierlaso213@hotmail.com>
2020-12-02 13:01:52 +0100

    refactored part of DaySchedule to a new component

--- client/src/components/Schedules/DaySchedule/DaySchedule.tsx
+++ client/src/components/Schedules/DaySchedule/DaySchedule.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { dayScheduleProps } from '../../../interfaces/interfaces'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import { dayScheduleProps, sessionInterface } from '../../../interfaces/interfaces'
+import HourItem from '../HourItem/HourItem'
 
 const { width } = Dimensions.get('window')
 
@@ -13,71 +14,34 @@ const styles = StyleSheet.create({
     textTransform: 'capitalize',
     textAlign: 'center'
   },
+  noScheduleText: {
+    fontSize: 24,
+    color: 'white',
+    marginTop: 10,
+    marginBottom: 20,
+    textAlign: 'center',
+    paddingHorizontal: 70,
+    lineHeight: 40
+  },
   dayView: {
     flex: 1,
-    width: width - 5,
+    width: width - 10,
     marginHorizontal: 5
-  },
-  hourText: {
-    color: 'white',
-    width: 150,
-    textAlign: 'center',
-    fontSize: 19
-  },
-  hourView: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    minHeight: 70,
-    marginVertical: 5
-  },
-  modifyButton: {
-    backgroundColor: '#14680c',
-    height: 40,
-    padding: 5,
-    borderRadius: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 30,
-    elevation: 3
-  },
-  modifyButtonText: {
-    color: 'white',
-    fontSize: 19
   }
 })
 
-function DaySchedule ({ item }: dayScheduleProps) {
+function DaySchedule ({ weekDay }: dayScheduleProps) {
   return (
     <View style={styles.dayView}>
-      <Text style={styles.dayText}>{item.day}</Text>
+      <Text style={styles.dayText}>{weekDay.day}</Text>
       <View>
-        {item && item.hours.map((hour) => {
-          return (
-          <View key={performance.now() * Math.random()} style={{
-            ...styles.hourView,
-            backgroundColor:
-            hour.type === 'WOD'
-              ? '#0061ba'
-              : hour.type === 'Open Box'
-                ? '#14680c'
-                : '#cb1313'
-          }} >
-            <View style={{ flex: 1 }}/>
-            <Text style={styles.hourText}>{`${hour.startHour}-${hour.finishHour}`}</Text>
-            <View style={{ flex: 2 }}/>
-            <Text style={styles.hourText}>{hour.type}</Text>
-            <View style={{ flex: 2 }}/>
-            <TouchableOpacity
-            style={styles.modifyButton}
-            onPress={() => alert(`Modifying ${hour.type} from ${item.day} at ${hour.startHour}!`)}
-            >
-              <Text style={styles.modifyButtonText}>Modify</Text>
-            </TouchableOpacity>
-            <View style={{ flex: 1 }}/>
-          </View>)
-        })}
+        {weekDay && (!weekDay.hours.length
+          ? <Text style={styles.noScheduleText}>There is no schedule for this day</Text>
+          : (weekDay.hours.map((session: sessionInterface) => {
+              return <HourItem day={weekDay.day} session={session} key={performance.now() * Math.random()} />
+            })
+            ))
+          }
       </View>
     </View>
   )
