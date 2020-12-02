115f1b500249dad2bb3663400169053e345a229f
Jheavi <javierlaso213@hotmail.com>
2020-12-02 20:31:58 +0100

    created updateSession action

--- client/src/redux/actions/schedulesActions.ts
+++ client/src/redux/actions/schedulesActions.ts
@@ -4,7 +4,7 @@ import serverUrls from '../../constants/serverUrls'
 import Action from './actionInterface'
 import { AppThunk } from '../reducers'
 import { AppDispatch } from '../configureStore'
-import { scheduleInterface } from '../../interfaces/interfaces'
+import { scheduleInterface, sessionInterface } from '../../interfaces/interfaces'
 
 export function loadSchedulesSuccess (schedules: [scheduleInterface]): Action {
   return {
@@ -28,6 +28,44 @@ export function loadSchedules (): AppThunk {
       dispatch(loadSchedulesSuccess(data))
     } catch (error) {
       dispatch(loadSchedulesError(error))
+    }
+  }
+}
+
+export function updateSessionSuccess (schedules: [scheduleInterface]): Action {
+  return {
+    type: actionTypes.UPDATE_SESSION,
+    schedules
+  }
+}
+
+export function updateSessionError (error: any): Action {
+  return {
+    type: actionTypes.UPDATE_SESSION_ERROR,
+    error
+  }
+}
+export function updateSession (
+  day: string,
+  session: sessionInterface,
+  finishHourValue: string,
+  startHourValue: string,
+  typeValue: string
+): AppThunk {
+  return async (dispatch: AppDispatch) => {
+    try {
+      const { data } = await axios.patch(`${serverUrls.scheduleUrl}/${day}`,
+        {
+          session,
+          finishHourValue,
+          startHourValue,
+          typeValue
+        }
+      )
+
+      dispatch(updateSessionSuccess(data))
+    } catch (error) {
+      dispatch(updateSessionError(error))
     }
   }
 }
