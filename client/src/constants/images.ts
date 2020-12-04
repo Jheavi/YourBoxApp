2464ede5da17eb2e706ca3a43b68379eed1293a9
Jheavi <javierlaso213@hotmail.com>
2020-12-04 16:47:00 +0100

    added constants images file

---
+++ client/src/constants/images.ts
@@ -0,0 +1,5 @@
+const images = {
+  homeScreen: 'https://trello-attachments.s3.amazonaws.com/5fc6ad22e8f1971a74d3b3a5/5fc6ade260f98805ecff5fd0/7ccd1dcd43eb5d424c3e06b094ced535/baby-beast-crossfit.jpg'
+}
+
+export default images
