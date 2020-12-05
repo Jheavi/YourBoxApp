aebbaae5a3ea48354d91729278a062a0e2c0c4e1
Jheavi <javierlaso213@hotmail.com>
2020-12-06 00:15:08 +0100

    refactored server urls

--- client/src/constants/serverUrls.ts
+++ client/src/constants/serverUrls.ts
@@ -1,6 +1,9 @@
+const baseUrl = 'http://192.168.1.136:2130'
+
 const serverUrls = {
-  workoutUrl: 'http://192.168.1.136:2130/workouts',
-  scheduleUrl: 'http://192.168.1.136:2130/schedules'
+  workoutUrl: `${baseUrl}/workouts`,
+  scheduleUrl: `${baseUrl}/schedules`,
+  userUrl: `${baseUrl}/users`
 }
 
 export default serverUrls
