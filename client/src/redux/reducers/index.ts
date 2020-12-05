e46257e70cc2b3455aa10c04e4f09ec6edcac29b
Jheavi <javierlaso213@hotmail.com>
2020-12-06 00:21:19 +0100

    added userReducer with cases for login and logout and added userReducer in combineReducers

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import workoutReducer from './workoutReducer'
 import schedulesReducer from './schedulesReducer'
+import userReducer from './userReducer'
 
 const rootReducer = combineReducers({
   workoutReducer,
-  schedulesReducer
+  schedulesReducer,
+  userReducer
 })
 
 export default rootReducer
