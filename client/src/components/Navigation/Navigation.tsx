a6a0569e489ffeac3d7535f6b09336d02f87872b
Jheavi <javierlaso213@hotmail.com>
2020-12-06 14:33:11 +0100

    created Navigation component to have navigation inside redux store

---
+++ client/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Workout from '../Workout/Workout'
+import Header from '../Header/Header'
+import AdminHome from '../HomeScreen/AdminHome'
+import Schedules from '../Schedules/Schedules'
+import Login from '../UserControl/Login/Login'
+import UserView from '../UserView/UserView'
+import { props } from '../../interfaces/interfaces'
+import Logout from '../UserControl/Logout/Logout'
+
+const { Navigator, Screen } = createStackNavigator()
+
+const header = {
+  header: () => <Header />
+}
+
+function Navigation ({ user }: props) {
+  return (
+    <NavigationContainer>
+      <Navigator initialRouteName={!user
+        ? 'Login'
+        : user.admin
+          ? 'Home'
+          : 'UserView'}>
+        {!user
+          ? <Screen
+              name="Login"
+              component={Login}
+              options={header}
+            />
+          : user.admin
+            ? <>
+              <Screen
+                name="Home"
+                component={AdminHome}
+                options={header}
+              />
+              <Screen
+                name="AdminWorkout"
+                component={Workout}
+                options={header}
+              />
+              <Screen
+                name="AdminSchedules"
+                component={Schedules}
+                options={header}
+              />
+              <Screen
+                name="Logout"
+                component={Logout}
+                options={header}
+              />
+            </>
+            : <>
+              <Screen
+                name="UserView"
+                component={UserView}
+                options={header}
+              />
+              <Screen
+                name="Logout"
+                component={Logout}
+                options={header}
+              />
+            </>
+        }
+      </Navigator>
+    </NavigationContainer>
+  )
+}
+
+function mapStateToProps ({ userReducer }: any) {
+  return {
+    user: userReducer.user
+  }
+}
+
+export default connect(mapStateToProps)(Navigation)
