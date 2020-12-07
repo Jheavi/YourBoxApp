62161d8c80ea93ab08a25780dd1718ab909ea7e7
Jheavi <javierlaso213@hotmail.com>
2020-12-07 18:37:55 +0100

    updated date functions

--- server/src/utils/dateFunctions.test.ts
+++ server/src/utils/dateFunctions.test.ts
@@ -1,4 +1,4 @@
-import { extractDataFromDate, extractDataFromTodayDate } from './dateFunctions'
+import { extractDataFromDate, sortByWeekDays } from './dateFunctions'
 
 describe('Date Functions', () => {
   const RealDate = Date.now
@@ -11,41 +11,117 @@ describe('Date Functions', () => {
     Date.now = RealDate
   })
 
-  test('extractDataFromDate should return an object with date info splitted', () => {
-    const date = extractDataFromDate('2020-09-22')
-
-    expect(date).toEqual({
-      day: 22,
-      month: 9,
-      year: 2020
-    })
-  })
-
-  test('extractDataFromTodayDate should return an object with month and date > 9', () => {
+  test('extractDataFromDate should return an object with month and date > 9', () => {
     // Mocked Date-now to return always the date 25/November/2020
     const now = 1606296219142
     Date.now = jest.spyOn(Date, 'now').mockImplementation(() => now)
-    const date = extractDataFromTodayDate()
+    const date = extractDataFromDate()
 
     expect(date).toEqual({
       day: 25,
       month: 11,
       year: 2020,
-      todayString: '2020-11-25'
+      dayString: '2020-11-25',
+      formattedDate: '25/11/2020',
+      weekDay: 'wednesday'
     })
   })
 
-  test('extractDataFromTodayDate should return an object with month and date < 10', () => {
+  test('extractDataFromDate should return an object with month and date < 10', () => {
     // Mocked Date-now to return always the date 02/September/2020
     const now = 1598999219142
     Date.now = jest.spyOn(Date, 'now').mockImplementation(() => now)
-    const date = extractDataFromTodayDate()
+    const date = extractDataFromDate()
 
     expect(date).toEqual({
       day: 2,
       month: 9,
       year: 2020,
-      todayString: '2020-09-02'
+      dayString: '2020-09-02',
+      formattedDate: '02/09/2020',
+      weekDay: 'wednesday'
     })
+  })
+
+  test('extractDataFromDate should return the date formatted if date provided', () => {
+    // Mocked Date-now to return always the date 02/September/2020
+    const date = extractDataFromDate('2020-09-02')
+
+    expect(date).toEqual({
+      day: 2,
+      month: 9,
+      year: 2020,
+      dayString: '2020-09-02',
+      formattedDate: '02/09/2020',
+      weekDay: 'wednesday'
+    })
+  })
+
+  test('extractDataFromDate should return weekDay = monday date is monday', () => {
+    const { weekDay } = extractDataFromDate('2020-12-07')
+
+    expect(weekDay).toBe('monday')
+  })
+
+  test('extractDataFromDate should return weekDay = tuesday date is tuesday', () => {
+    const { weekDay } = extractDataFromDate('2020-12-08')
+
+    expect(weekDay).toBe('tuesday')
+  })
+
+  test('extractDataFromDate should return weekDay = wednesday if date is wednesday', () => {
+    const { weekDay } = extractDataFromDate('2020-12-09')
+
+    expect(weekDay).toBe('wednesday')
+  })
+
+  test('extractDataFromDate should return weekDay = thursday if date is thursday', () => {
+    const { weekDay } = extractDataFromDate('2020-12-10')
+
+    expect(weekDay).toBe('thursday')
+  })
+
+  test('extractDataFromDate should return weekDay = friday if date is friday', () => {
+    const { weekDay } = extractDataFromDate('2020-12-11')
+
+    expect(weekDay).toBe('friday')
+  })
+
+  test('extractDataFromDate should return weekDay = saturday if date is saturday', () => {
+    const { weekDay } = extractDataFromDate('2020-12-12')
+
+    expect(weekDay).toBe('saturday')
+  })
+
+  test('extractDataFromDate should return weekDay = sunday if date is sunday', () => {
+    const { weekDay } = extractDataFromDate('2020-12-13')
+
+    expect(weekDay).toBe('sunday')
+  })
+
+  test('extractDataFromDate should return weekDay = error if date is error', () => {
+    const { weekDay } = extractDataFromDate('fakedate')
+
+    expect(weekDay).toBe('error')
+  })
+
+  test('sortByWeekDays should return 1 if dayOne.day is tuesday and dayTwo.day is monday', () => {
+    // Mocked Date-now to return always the date 02/September/2020
+    const dayOne = { day: 'tuesday', sessions: [] }
+    const dayTwo = { day: 'monday', sessions: [] }
+
+    const response = sortByWeekDays(dayOne, dayTwo)
+
+    expect(response).toBe(1)
+  })
+
+  test('sortByWeekDays should return -1 if dayOne.day is monday and dayTwo.day is tuesday', () => {
+    // Mocked Date-now to return always the date 02/September/2020
+    const dayOne = { day: 'monday', sessions: [] }
+    const dayTwo = { day: 'tuesday', sessions: [] }
+
+    const response = sortByWeekDays(dayOne, dayTwo)
+
+    expect(response).toBe(-1)
   })
 })
--- server/src/utils/dateFunctions.ts
+++ server/src/utils/dateFunctions.ts
@@ -1,18 +1,55 @@
-import { dateObject } from '../interfaces/interfaces'
+import { scheduleInterface, dateObject } from '../interfaces/interfaces'
 
-export function extractDataFromTodayDate (): dateObject {
-  const today: Date = new Date(Date.now())
-  const day: number = today.getDate()
+export function extractDataFromDate (date?: string): dateObject {
+  const dayToFormat: Date = new Date(date || Date.now())
+  const day: number = dayToFormat.getDate()
   const dayString: string = day > 9 ? `${day}` : `0${day}`
-  const month: number = today.getMonth() + 1
+  const month: number = dayToFormat.getMonth() + 1
   const monthString: string = month > 9 ? `${month}` : `0${month}`
-  const year: number = today.getFullYear()
+  const year: number = dayToFormat.getFullYear()
+  const weekDayNumber: number = dayToFormat.getDay()
+  let weekDay: string
+  switch (weekDayNumber) {
+    case 0:
+      weekDay = 'sunday'
+      break
+    case 1:
+      weekDay = 'monday'
+      break
+    case 2:
+      weekDay = 'tuesday'
+      break
+    case 3:
+      weekDay = 'wednesday'
+      break
+    case 4:
+      weekDay = 'thursday'
+      break
+    case 5:
+      weekDay = 'friday'
+      break
+    case 6:
+      weekDay = 'saturday'
+      break
+    default:
+      weekDay = 'error'
+      break
+  }
 
-  return { day, month, year, todayString: `${year}-${monthString}-${dayString}` }
+  return {
+    day,
+    month,
+    year,
+    dayString: `${year}-${monthString}-${dayString}`,
+    formattedDate: `${dayString}/${monthString}/${year}`,
+    weekDay
+  }
 }
 
-export function extractDataFromDate (date: string | undefined): dateObject {
-  const [year, month, day] = date!.split('-')
+export function sortByWeekDays (dayOne: scheduleInterface, dayTWo: scheduleInterface) {
+  const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 
-  return { year: +year, month: +month, day: +day }
+  const dayOneIndex = days.indexOf(dayOne.day)
+  const dayTWoIndex = days.indexOf(dayTWo.day)
+  return dayOneIndex > dayTWoIndex ? 1 : -1
 }
