0a62e6ad0ff8f9a5457ab138df6e10a5be4bfecb
Jheavi <javierlaso213@hotmail.com>
2020-12-07 14:24:52 +0100

    removed onLogin function and test

--- client/src/utils/authFunctions.test.ts
+++ client/src/utils/authFunctions.test.ts
@@ -1,6 +1,4 @@
-import { toQueryString, onLogin } from './authFunctions'
-import * as AuthSession from 'expo-auth-session'
-import { Platform } from 'react-native'
+import { toQueryString } from './authFunctions'
 
 jest.mock('react-native')
 jest.mock('expo-auth-session')
@@ -12,15 +10,5 @@ describe('authFunctions', () => {
     const response = toQueryString(params)
 
     expect(response).toBe('?param1=value1&param2=value2')
-  })
-
-  test('onLogin should call Platform.select', async () => {
-    const fakeResponse = { type: 'cancel' }
-    Platform.select = jest.fn()
-    const spyMakeRedirectUri = jest.spyOn(AuthSession, 'makeRedirectUri').mockReturnValueOnce('url')
-    const spyStartAsync = jest.spyOn(AuthSession, 'startAsync').mockResolvedValueOnce(fakeResponse)
-    const response = await onLogin()
-
-    expect(response).toEqual(fakeResponse)
   })
 })
--- client/src/utils/authFunctions.ts
+++ client/src/utils/authFunctions.ts
@@ -10,26 +10,6 @@ export function toQueryString (params: StringMap) {
   return `?${paramsJoined}`
 };
 
-export async function onLogin () {
-  const useProxy = Platform.select({ web: false, default: true })
-  const redirectUri = makeRedirectUri({ native: auth0data.redirectUri, useProxy })
-
-  const params = {
-    client_id: auth0data.clientId,
-    redirect_uri: redirectUri,
-    response_type: 'token id_token',
-    nonce: 'nonce',
-    prompt: 'select_account'
-  }
-
-  const queryParams = toQueryString(params)
-
-  const authUrl = `https://${auth0data.domain}/authorize${queryParams}`
-
-  const response = await startAsync({ authUrl, showInRecents: true })
-  return response
-}
-
 export async function onLogout () {
   const useProxy = Platform.select({ web: false, default: true })
   const redirectUri = makeRedirectUri({ native: auth0data.logoutUri, useProxy })
