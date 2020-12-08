7183c2e89b64735eb35d19e4d1156788f8abcfef
Jheavi <javierlaso213@hotmail.com>
2020-12-08 14:15:24 +0100

    added checks to disable hours already passed

--- client/src/components/UserSchedules/UserSessionItem/UserSessionItem.tsx
+++ client/src/components/UserSchedules/UserSessionItem/UserSessionItem.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { connect } from 'react-redux'
 import { props } from '../../../interfaces/interfaces'
 import { addOrRemoveReservedSession } from '../../../redux/actions/userActions'
+import { extractDataFromDate } from '../../../utils/dateFunctions'
 
 const styles = StyleSheet.create({
   sessionView: {
@@ -20,10 +21,11 @@ const styles = StyleSheet.create({
     color: 'white',
     width: 150,
     textAlign: 'center',
-    fontSize: 19
+    fontSize: 19,
+    marginLeft: 15
   },
   enrollButton: {
-    backgroundColor: '#14680c',
+    backgroundColor: '#218520',
     height: 40,
     paddingVertical: 5,
     paddingHorizontal: 10,
@@ -31,6 +33,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginHorizontal: 30,
+    width: 80,
     elevation: 8
   },
   enrollButtonText: {
@@ -44,7 +47,21 @@ const styles = StyleSheet.create({
 })
 
 function UserSessionItem ({ day, dispatch, session, user }: props) {
-  function checkIfUserHasSession () {
+  const actualDay = extractDataFromDate()
+
+  function checkIfSessionHasPassed (): boolean {
+    if (actualDay.dayString > day) {
+      return true
+    } else if (actualDay.dayString < day) {
+      return false
+    } else {
+      return +actualDay.hour.split(':')[0] + 2 > +session.startHour.split(':')[0]
+    }
+  }
+
+  const [sessionHasPassed] = useState(checkIfSessionHasPassed())
+
+  function checkIfUserHasSession (): boolean {
     return user.reservedSessions.some((reservedSession) => (
       reservedSession.day === day &&
       reservedSession.startHour === session.startHour &&
@@ -72,12 +89,13 @@ function UserSessionItem ({ day, dispatch, session, user }: props) {
       ...styles.sessionView,
       backgroundColor:
       userHasSession
-        ? '#b4d8fa'
+        ? '#94b8da'
         : session!.type === 'WOD'
           ? '#014aa5'
           : session!.type === 'Open Box'
             ? '#016500'
-            : '#a20000'
+            : '#a20000',
+      opacity: !sessionHasPassed ? 1 : 0.5
     }} >
       <View style={{ flex: 1 }}/>
       <Text style={styles.sessionText} testID="hourText">{`${session!.startHour} - ${session!.finishHour}`}</Text>
@@ -89,6 +107,7 @@ function UserSessionItem ({ day, dispatch, session, user }: props) {
           style={styles.enrollButton}
           onPress={OnEnrollPress}
           testID="enrollBtn"
+          disabled={sessionHasPassed}
         >
           <Text style={styles.enrollButtonText}>Enroll</Text>
         </TouchableOpacity>
@@ -98,6 +117,7 @@ function UserSessionItem ({ day, dispatch, session, user }: props) {
           style={{ ...styles.enrollButton, backgroundColor: '#cb1313' }}
           onPress={OnCancelPress}
           testID="cancelBtn"
+          disabled={sessionHasPassed}
         >
           <Text style={styles.enrollButtonText}>Cancel</Text>
         </TouchableOpacity>
