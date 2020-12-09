bc155c10e4a1f553c93a8688884b03422c14c1e8
Jheavi <javierlaso213@hotmail.com>
2020-12-09 13:57:19 +0100

    added image

--- client/src/constants/images.ts
+++ client/src/constants/images.ts
@@ -1,7 +1,8 @@
 const images = {
   homeScreen: { uri: 'https://trello-attachments.s3.amazonaws.com/5fc6ad22e8f1971a74d3b3a5/5fc6ade260f98805ecff5fd0/7ccd1dcd43eb5d424c3e06b094ced535/baby-beast-crossfit.jpg' },
   workoutbackground: { uri: 'https://trello-attachments.s3.amazonaws.com/5fc6ad22e8f1971a74d3b3a5/5fc6ade260f98805ecff5fd0/8174a262d41dc582d175bf5e3adf8685/clean.jpeg' },
-  gym1: { uri: 'https://trello-attachments.s3.amazonaws.com/5fc6ad22e8f1971a74d3b3a5/5fc6ade260f98805ecff5fd0/32f98be2f58ff7ec00486625017d502b/gym1-image2.jpg' }
+  gym1: { uri: 'https://trello-attachments.s3.amazonaws.com/5fc6ad22e8f1971a74d3b3a5/5fc6ade260f98805ecff5fd0/32f98be2f58ff7ec00486625017d502b/gym1-image2.jpg' },
+  haltero1: { uri: 'https://trello-attachments.s3.amazonaws.com/5fc6ad22e8f1971a74d3b3a5/5fc6ade260f98805ecff5fd0/9fcdc69c4ec741e63e58d557487b65ca/haltero1.jpg' }
 }
 
 export default images
