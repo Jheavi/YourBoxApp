99b2cb246b16c428310726eff82529b5d1f5902f
Jheavi <javierlaso213@hotmail.com>
2020-12-09 13:39:05 +0100

    added gym view in login compo

--- client/src/components/UserControl/Login/Login.tsx
+++ client/src/components/UserControl/Login/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet, ImageBackground, Platform, Alert } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground, Platform, Alert, ScrollView, Dimensions } from 'react-native'
 import images from '../../../constants/images'
 import { login } from '../../../redux/actions/userActions'
 import { connect } from 'react-redux'
@@ -7,18 +7,30 @@ import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import auth0data from '../../../constants/auth0data'
 import { props } from '../../../interfaces/interfaces'
 
+const { width } = Dimensions.get('window')
+
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     backgroundColor: '#0d0d0d',
     flex: 1,
     justifyContent: 'center',
-    position: 'relative'
+    position: 'relative',
+    width
   },
-  buttonView: {
+  scrollContent: {
+    flex: 1,
+    flexGrow: 1,
+    backgroundColor: '#0d0d0d',
+    width: '100%'
+  },
+  loginButton: {
     backgroundColor: '#cb1313',
     padding: 10,
-    margin: 30,
+    marginVertical: 40,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: 130,
     borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
@@ -28,6 +40,12 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 30
   },
+  secondaryTitles: {
+    color: 'white',
+    fontSize: 25,
+    marginLeft: 30,
+    marginBottom: 15
+  },
   backImage: {
     position: 'absolute',
     top: 0,
@@ -36,10 +54,57 @@ const styles = StyleSheet.create({
     height: '100%',
     resizeMode: 'cover',
     opacity: 0.4
+  },
+  gymView: {
+    zIndex: 10,
+    flex: 1,
+    maxHeight: 200,
+    width: '100%',
+    position: 'relative'
+  },
+  triangleShape: {
+    zIndex: 10,
+    position: 'absolute',
+    width: 0,
+    height: 0,
+    right: 0,
+    bottom: 0,
+    borderLeftWidth: 220,
+    borderBottomWidth: 60,
+    backgroundColor: 'transparent',
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: '#000000'
+  },
+  gymBackImage: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover'
+  },
+  schedulesButton: {
+    backgroundColor: 'transparent',
+    borderColor: 'white',
+    borderWidth: 4,
+    borderRadius: 100,
+    position: 'absolute',
+    fontSize: 20,
+    bottom: 20,
+    width: '50%',
+    marginLeft: 15,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden'
+  },
+  schedulesButtonText: {
+    color: 'white',
+    fontSize: 24
   }
 })
 
-function Login ({ dispatch }: props) {
+function Login ({ dispatch, navigation }: props) {
   const useProxy = Platform.select({ web: false, default: true })
   const redirectUri = makeRedirectUri({ useProxy })
 
@@ -73,16 +138,33 @@ function Login ({ dispatch }: props) {
 
   return (
     <View style={styles.container}>
-      <ImageBackground
-        style={styles.backImage}
-        source={images.homeScreen}
-      />
-      <TouchableOpacity
-        style={styles.buttonView}
-        onPress={() => promptAsync({ useProxy })}
-      >
-        <Text style={styles.buttonsText}>Login</Text>
-      </TouchableOpacity>
+      <ScrollView style={{ width: '100%' }} contentContainerStyle={styles.scrollContent}>
+        {/* <ImageBackground
+          style={styles.backImage}
+          source={images.homeScreen}
+        /> */}
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => promptAsync({ useProxy })}
+        >
+          <Text style={styles.buttonsText}>Login</Text>
+        </TouchableOpacity>
+        <Text style={styles.secondaryTitles}>See gyms around you</Text>
+        <View style={styles.gymView}>
+          <ImageBackground
+            style={styles.gymBackImage}
+            source={images.gym1}
+          />
+          <View style={styles.triangleShape}/>
+          <TouchableOpacity
+            style={styles.schedulesButton}
+            activeOpacity={0.4}
+            onPress={() => navigation.navigate('AdminSchedules')}
+          >
+            <Text style={styles.schedulesButtonText}>See schedules</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   )
 }
