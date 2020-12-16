4506559de404680b37a018ebd99bfe2126636811
Jheavi <javierlaso213@hotmail.com>
2020-12-16 12:28:40 +0100

    error fixed

--- client/src/components/Schedules/DaySchedule/__mocks__/DaySchedule.tsx
+++ client/src/components/Schedules/DaySchedule/__mocks__/DaySchedule.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Text } from 'react-native'
 
 function DaySchedule () {
-  return <View />
+  return <Text>MockedDaySchedule</Text>
 }
 
 export default DaySchedule
