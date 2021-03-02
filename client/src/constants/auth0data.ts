65a81edd424cf6a1c90a8ebf4c1c5b3309ecd38d
Jheavi <javierlaso213@hotmail.com>
2021-03-02 16:57:46 +0100

    fix env variables

--- client/src/constants/auth0data.ts
+++ client/src/constants/auth0data.ts
@@ -1,8 +1,15 @@
+import {
+  AUTH0_DOMAIN,
+  AUTH0_CLIENTID,
+  AUTH0_REDIRECTURL,
+  AUTH0_LOGOUTURL
+} from '@env'
+
 const auth0data = {
-  domain: 'javierlasogymapp.eu.auth0.com',
-  clientId: 'mGtaMlEolzco2eZAlpDld8F8sUAW3wjV',
-  redirectUri: 'gymapp://javierlasogymapp.eu.auth0.com/android/gymapp/callback',
-  logoutUri: 'javierlasogymapp.eu.auth0.com/android/gymapp/callback'
+  domain: AUTH0_DOMAIN,
+  clientId: AUTH0_CLIENTID,
+  redirectUri: AUTH0_REDIRECTURL,
+  logoutUri: AUTH0_LOGOUTURL
 }
 
 export default auth0data
---
+++ client/types/env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+  export const AUTH0_DOMAIN: string
+  export const AUTH0_CLIENTID: string
+  export const AUTH0_REDIRECTURL: string
+  export const AUTH0_LOGOUTURL: string
+}
